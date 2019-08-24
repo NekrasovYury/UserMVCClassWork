8145308aae16efb25933b8c5fb9bb33a90641ad3
Юрий Некрасов <furynoobas@gmail.com>
2019-08-24 12:05:25 +0300

    Add Unity And Identity

--- ClassApp/App_Start/IdentityConfig.cs
+++ ClassApp/App_Start/IdentityConfig.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNet.Identity.Owin;
+﻿using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
 using Owin;
 
 [assembly: OwinStartup(typeof(ClassApp.App_Start.IdentityConfig))]
@@ -11,12 +13,21 @@ namespace ClassApp.App_Start
     {
         public void Configuration(IAppBuilder appBuilder)
         {
-            appBuilder.CreatePerOwinContext(() => new IdentityDbContext());
+            appBuilder.CreatePerOwinContext(() => DependencyResolver.Current.GetService<UserManager<IdentityUser>>());
+            appBuilder.CreatePerOwinContext(() => DependencyResolver.Current.GetService<RoleManager<IdentityRole>>());
 
-            appBuilder.CreatePerOwinContext<UserManager<IdentityUser>>((o,c) => new UserManager<IdentityUser>(c.Get<IUserStore<IdentityUser>>()));
+            //appBuilder.CreatePerOwinContext<UserManager<IdentityUser>>((options, context) => new UserManager<IdentityUser>(context.Get<IUserStore<IdentityUser>>()));
 
-            appBuilder.CreatePerOwinContext<RoleManager<IdentityRole>>((options, context))
-            
+            //appBuilder.CreatePerOwinContext<RoleManager<IdentityRole>>((options, context) =>
+            //    new RoleManager<IdentityRole>(
+            //        new RoleStore<IdentityRole>(context.Get<IdentityDbContext>())));
+
+            appBuilder.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/home/login"),
+            });
+
         }
     }
 }
---
+++ ClassApp/App_Start/UnityConfig.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Unity;
+using Unity.Injection;
+using Unity.Lifetime;
+
+namespace ClassApp.App_Start
+{
+    public class UnityConfig
+    {
+        private static Lazy<IUnityContainer> container = new Lazy<IUnityContainer>(() =>
+        {
+            var container = new UnityContainer();
+            RegisterType(container);
+            return container;
+        });
+
+        public static IUnityContainer GetConfiguresContainer()
+        {
+            return container.Value;
+        }
+
+        public static void RegisterType(IUnityContainer container)
+        {
+            container.RegisterType<IdentityDbContext>(new HierarchicalLifetimeManager());
+
+            // user manager
+            container.RegisterType<IUserStore<IdentityUser>, UserStore<IdentityUser>>(
+                new HierarchicalLifetimeManager(),
+                new InjectionConstructor(
+                    new ResolvedParameter<IdentityDbContext>()
+                    ));
+
+            container.RegisterType<UserManager<IdentityUser>>(
+                new HierarchicalLifetimeManager());
+
+            //role manager
+            //
+            container.RegisterType<IRoleStore<IdentityRole, string>, RoleStore<IdentityRole>>(
+                new HierarchicalLifetimeManager(),
+                new InjectionConstructor(
+                    new ResolvedParameter<IdentityDbContext>()
+                ));
+
+            container.RegisterType<RoleManager<IdentityRole>>(
+                new HierarchicalLifetimeManager());
+        }
+    }
+}
